871b51ad6d58a3db6efc8a75af26b22a2b764ec7
Raushan Raj <90108340+raushanraj9427@users.noreply.github.com>
2023-09-15 00:53:18 +0530

    correct http exception statement

--- app/main.py
+++ app/main.py
@@ -39,7 +39,7 @@ async def read_plant(plant_id: int, db: Session = Depends(get_db)):
     ):
         return result
     else:
-        raise HTTPException(status_code=404, detail='Questions is not found')
+        raise HTTPException(status_code=404, detail='Plant not found')
 
 @app.get("/details/(plant_id)")
 async def read_detail(plant_id:int, db: Session = Depends(get_db)):
@@ -50,11 +50,11 @@ async def read_detail(plant_id:int, db: Session = Depends(get_db)):
     ):
         return result
     else:
-        raise HTTPException(status_code=404, detail='Questions is not found')
+        raise HTTPException(status_code=404, detail='Details not found')
 
 @app.post("/plants/")
 async def create_plant(plant_text:str, db: db_dependency):
-    db_plant = models.Plants(plant_text = plant_text)
+    db_plant = Plants(plant_text = plant_text)
     db.add(db_plant)
     db.commit()
     db.refresh(db_plant)
@@ -62,11 +62,11 @@ async def create_plant(plant_text:str, db: db_dependency):
 
 @app.post("/plants_details/")
 async def create_plant_details(plant: PlantBase, db: db_dependency):
-    db_plant = models.Plants(plant_text = plant.plant_text)
+    db_plant = Plants(plant_text = plant.plant_text)
     db.add(db_plant)
     db.commit()
     db.refresh(db_plant)
     for choice in plant.choices:
-        db_choice = models.Details(plant_family=choice.plant_family, plant_bio=choice.plant_bio, plant_descr=choice.plant_descr, plant_url= choice.plant_url, plant_id=db_plant.id)
+        db_choice = Details(plant_family=choice.plant_family, plant_bio=choice.plant_bio, plant_descr=choice.plant_descr, plant_url= choice.plant_url, plant_id=db_plant.id)
         db.add(db_choice)
     db.commit()
